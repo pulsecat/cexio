f166a00d391805fec32231850f241959c0cb1931
Pulse Cat <admin@cryptotrader.org>
2013-11-28 03:59:14 +0700

    fix request timeout bug

--- cexio.js
+++ cexio.js
@@ -65,9 +65,9 @@ CEXIO.prototype._request = function(method, path, data, callback, args) {
     res.on('error', function(err) {
       callback(err);
     });
-    req.setTimeout(60000,function() {
-      req.abort();
-    });
+  });
+  req.setTimeout(60000,function() {
+    req.abort();
   });
   req.end(data);
 }
